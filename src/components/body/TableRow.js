8b540247fffdcec437fe92b2ccdb504e01a42ed9
kunal528 <jainkunal976@gmail.com>
2021-02-07 12:48:23 +0530

    uniswap , sushiswap and balancer implemented in front-end

--- src/components/body/TableRow.js
+++ src/components/body/TableRow.js
@@ -3,6 +3,13 @@ import SwapIcons from "./SwapIcons";
 import TwitterIcons from "./TwitterIcons";
 import Image from "./Image";
 const TableRow = ({ swap, state }) => {
+
+  const imgUrl =
+    swap.dexName === "Uniswap"
+      ? "https://etherscan.io/images/uniswap_ta.png"
+      : swap.dexName === "Sushiswap"
+      ? "https://etherscan.io/images/defi/sushiswap.svg"
+      : "https://etherscan.io/images/svg/brands/balancer.png"; 
   return (
     <div style={{ backgroundColor: "white" }} className={`table-row`}>
       <div style={{ content: `${swap.tx_id}`, width: "11%" }}>
@@ -26,7 +33,7 @@ const TableRow = ({ swap, state }) => {
         />
       </div>
       <div style={{ width: "14%" }}>{swap.amountIn}</div>
-      <div style={{ width: "14%" }}>{swap.amountOut}</div>
+      <div style={{ width: "14%" }}>{swap.amountOut}<br/> on <img src={imgUrl} style={{ width:'10px' , height:'10px' }} alt="Error" /> { swap.dexName}</div>
       <div style={{ width: "14%" }}>{swap.with1inch}</div>
       <div style={{ width: "14%" }}>{swap.loss}</div>
       <div style={{ width: "7%" }}>
